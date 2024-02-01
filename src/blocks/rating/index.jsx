ebdcec8ac45e474367cda5b7846013ec621fd3bb
Sosin Dima <sosindima5@gmail.com>
2024-02-01 16:34:26 +0700

    create stars

--- src/blocks/rating/index.jsx
+++ src/blocks/rating/index.jsx
@@ -1,20 +1,23 @@
 import styles from './styles.module.css'
-import Star from '../../assets/star.svg'
+import {Star} from './star.jsx'
 
 export function Rating({num}) {
-  let star = Math.trunc(num/2);
+
+  let star = Math.round(num/2);
+
+  let arr = [];
+  for (let i = 0; i < star; i++)
+    arr.push(<Star color={"#FFB219"}/>);
+  for (let i = star; i < 5; i++)
+    arr.push(<Star color={"#CED2DA"}/>);
 
   return (
     <div className={styles.rating}>
       <div className={styles.stars}>
-          <img src={Star} alt='star' />
-          <img src={Star} alt='star' />
-          <img src={Star} alt='star' />
-          <img src={Star} alt='star' />
-          <img src={Star} alt='star' />
+        {arr}
       </div>
       <div className={styles.score}>
-          <p>Kinopoisk - {num}</p>
+        <p>Kinopoisk - {num}</p>
       </div>
     </div>
   )
