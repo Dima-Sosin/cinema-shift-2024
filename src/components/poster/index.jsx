e5f4a5f9b1af8d8f400a7e28f46e9cc042fcf97e
Sosin Dima <sosindima5@gmail.com>
2024-02-02 15:51:45 +0700

    added "genre", "country" and "year" to the poster

--- src/components/poster/index.jsx
+++ src/components/poster/index.jsx
@@ -4,10 +4,20 @@ import { Button } from "../button"
 import { baseUrl } from "../../api/request"
 
 export function Poster({ film }) {
+   const year = () => {
+      const len = film.releaseDate.length
+      const str = film.releaseDate.slice(len-4)
+      return str
+   }
+   
    return (
       <div className={styles.poster}>
          <div className={styles.img}>
             <img src={baseUrl + film.img} alt="poster"></img>
+            <div className={styles.block}>
+               <p className={styles.genre}>{film.genres[0]}</p>
+               <p className={styles.country}>{film.country.name}, {year()}</p>
+            </div>
          </div>
          <h2 className={styles.title}>{film.name}</h2>
          <p className={styles.subtitle}>{film.description}</p>
--- src/components/rating/index.jsx
+++ src/components/rating/index.jsx
@@ -5,8 +5,8 @@ export function Rating({ num }) {
    const stars = () => {
       let star = Math.round(num / 2)
       let arr = []
-      for (let i = 0; i < star; i++) arr.push(<Star color={"#FFB219"} />) //золотые звезды
-      for (let i = star; i < 5; i++) arr.push(<Star color={"#CED2DA"} />) //серые звезды
+      for (let i = 0; i < star; i++) arr.push(<Star color={"#FFB219"} key={i}/>) //золотые звезды
+      for (let i = star; i < 5; i++) arr.push(<Star color={"#CED2DA"} key={i}/>) //серые звезды
       return arr
    }
 
