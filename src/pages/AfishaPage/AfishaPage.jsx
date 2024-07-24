e60ced7e3dd7dd29ba7f51baef955d2ecd43949e
Dima-Sosin <sosindima5@gmail.com>
2024-07-24 22:21:56 +0700

    не шарю

--- src/pages/AfishaPage/AfishaPage.jsx
+++ src/pages/AfishaPage/AfishaPage.jsx
@@ -7,9 +7,7 @@ export const AfishaPage = () => {
     const films = useLoaderData().films;
     return (
         <PageLayout>
-            <h2>
-                Афиша
-            </h2>
+            <h2>Афиша</h2>
             <ul className={styles.cards}>
                 {films.map((film) => (
                     <li key={film.id}>
