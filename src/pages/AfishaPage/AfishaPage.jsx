4e1567ed88c6676c4db254a2e0ec206e5130cec5
Dima-Sosin <sosindima5@gmail.com>
2024-07-24 21:25:05 +0700

    Все еще исправляю lint staged

--- src/pages/AfishaPage/AfishaPage.jsx
+++ src/pages/AfishaPage/AfishaPage.jsx
@@ -1,14 +1,13 @@
-import styles from "./AfishaPage.module.scss"
+import styles from "./AfishaPage.module.scss";
 import { useLoaderData } from "react-router-dom";
 import { Poster } from "./Poster/Poster";
-import { PageLayout } from "../../components/PageLayout/PageLayout"
+import { PageLayout } from "../../components/PageLayout/PageLayout";
 
 export const AfishaPage = () => {
     const films = useLoaderData().films;
     return (
         <PageLayout>
-            <h2>
-                Афиша
+            <h2>Афиша
             </h2>
             <ul className={styles.cards}>
                 {films.map((film) => (
